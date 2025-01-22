0d15570d3365f85ea54a3d3ef415b12a9ee1b4ff
Ajama0 <abasjama04@hotmail.com>
2025-01-22 19:21:10 +0000

    switch user authentication to use reactive forms

--- Dormly-Frontend/src/app/components/login/login.component.ts
+++ Dormly-Frontend/src/app/components/login/login.component.ts
@@ -1,23 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HomeComponent } from "../home/home.component";
 import {MatInputModule} from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { FormArray, ReactiveFormsModule, FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, 
+  imports: [ 
           CommonModule, 
           HomeComponent, 
           MatInputModule,
-          MatButtonModule
+          MatButtonModule,
+          ReactiveFormsModule,
+          FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  userForm : FormGroup
+
+
+  constructor(private formBuilder : FormBuilder){
+    this.userForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      passowrd: ['', Validators.maxLength(15), Validators.required]
+    })
+  }
 
 
   onSubmit(){

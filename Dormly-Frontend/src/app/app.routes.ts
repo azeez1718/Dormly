9f2848eecf7b201f3e371fee8142727f02561273
Ajama0 <abasjama04@hotmail.com>
2025-01-22 15:02:22 +0000

    add html for the frontend

--- Dormly-Frontend/src/app/app.routes.ts
+++ Dormly-Frontend/src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 ;
 
 export const routes: Routes = [
 
+    {
+        path : "",
+        component : HomeComponent
+
+    },
+
+    {
+        path : "login",
+        component : LoginComponent
+    }
 ];

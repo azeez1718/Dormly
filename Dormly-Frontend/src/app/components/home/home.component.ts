65f4c8c6f17de1dad24fc63d1514bc833992afb9
Ajama0 <abasjama04@hotmail.com>
2025-01-22 17:48:06 +0000

    add angular materials

--- Dormly-Frontend/src/app/components/home/home.component.ts
+++ Dormly-Frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, MatSlideToggleModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- Dormly-Frontend/src/app/components/login/login.component.ts
+++ Dormly-Frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from "../home/home.component";
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [FormsModule, 
+          CommonModule, 
+          HomeComponent, 
+          MatInputModule,
+          MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+
+
+  onSubmit(){
+    console.log("submitted")
+
+  }
 
 }

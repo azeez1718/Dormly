2ea2dc3861daa24218496b6ae2d211329d774c0f
Ajama0 <abasjama04@hotmail.com>
2025-01-25 13:28:09 +0000

    handle how cors is being configured

--- Dormly-Backend/src/main/java/com/example/Dormly/security/configuration/CorsConfig.java
+++ Dormly-Backend/src/main/java/com/example/Dormly/security/configuration/CorsConfig.java
@@ -1,27 +1,36 @@
 package com.example.Dormly.security.configuration;
 
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig{
 
     /**
      * as the interface contains Default methods from java 8 we can override and provide implementation for whatever methods we wabt
      * the Cors allows both servers to communicate as we define the origin as allowed HTTP methods
      */
 
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**") //our client side can send requests to all endpoints
-                .allowedOrigins("http://localhost:55567")
-                .allowedMethods("GET", "PUT", "POST", "DELETE")
-                .allowedHeaders("*") //allow client to have request include headers like Authorizaiton which includes the token
-                .allowCredentials(true);//allow cookies
 
-
+    @Bean
+    public WebMvcConfigurer configurer(){
+        //returns an anonymous class that implements webmvc
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") //our client side can send requests to all endpoints
+                        .allowedOrigins("http://localhost:55567")
+                        .allowedMethods("GET", "PUT", "POST", "DELETE")
+                        .allowedHeaders("*") //allow client to have request include headers like Authorizaiton which includes the token
+                        .allowCredentials(true);//allow cookies
+            }
+        };
     }
+
+
 
 
 }

0ac9eda165fd7e9967f52b75340f7cac20d83357
Ajama0 <abasjama04@hotmail.com>
2025-01-28 20:15:34 +0000

    dont extract token for finding user profile, use better practice within spring security

--- Dormly-Backend/src/main/java/com/example/Dormly/controller/ProfileController.java
+++ Dormly-Backend/src/main/java/com/example/Dormly/controller/ProfileController.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +21,15 @@ public class ProfileController {
     private final ProfileService profileService;
     private final JwtService jwtService;
 
-
+    /**
+     * the user sends a request, the filters validate the token
+     * we set an authentication object of the user before the dispatcher servlet delegates request to the controller
+     * @return ProfileDto - which is used to hide internals when sending back profile information
+     *
+     */
     @GetMapping(path = "my-account")
-    public ResponseEntity<ProfileDto> fetchUserProfile(HttpServletRequest Request){
-        String userEmail = jwtService.retrieveUserFromRequest(Request);
-        return new ResponseEntity<>(profileService.fetchProfile(userEmail), HttpStatus.OK);
+    public ResponseEntity<ProfileDto> fetchUserProfile(@AuthenticationPrincipal UserDetails userDetails){;
+        return new ResponseEntity<>(profileService.fetchProfile(userDetails.getUsername()), HttpStatus.OK);
 
     }
 }

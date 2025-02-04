910dca541d20428cea5893867247c17d0304323c
ajama0 <abasjama04@hotmail.com>
2025-02-04 17:23:43 +0300

    set up template and convert json into a blob and set content type

--- Dormly-Frontend/src/app/listing/listing.component.ts
+++ Dormly-Frontend/src/app/listing/listing.component.ts
@@ -1,28 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FileuploadService } from '../service/fileuploads/fileupload.service';
+import { ListingService } from '../service/listing/listing.service';
+import { Listing } from '../models/listing';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listing',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './listing.component.html',
   styleUrl: './listing.component.css'
 })
 export class ListingComponent implements OnInit{
   
-  
-  
-  
-  
-  constructor(){}
-  
-  
+  listingDetails!: Listing;
+
+  constructor(private fileService:FileuploadService, private listingService:ListingService){}
   
   
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.uploadListing
   }
 
   uploadListing(event:Event):void{
-    console.log("file selected")
+    //this will return the formdata that includes the file uploaded
+    //the backend expects a json and a file 
+    //multipart formdata allows us to send objects of different contents, the backend binds each key value to the method parameter
+    //we will sent a json which is the listing information and the image of the item itself
+    const formdata = this.fileService.uploadFile(event)
+    //wrapping the json in a blob allows to explicity define the content-type
+    const listingJson = new Blob([JSON.stringify(this.listingDetails)], {type:'application/json'})
+    formdata.append('listing', listingJson)
+
+    this.listingService.uploadlistingItems().subscribe({
+      next:(data:any)=>{
+        console.log("items uploaded successfully")
+      },
+      error:(error:Error)=>{
+        console.log(error.message)
+      }
+    })
+    
+  
   }
 
 }

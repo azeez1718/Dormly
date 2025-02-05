589141aff99c6574aff250b544d2215faa10b060
ajama0 <abasjama04@hotmail.com>
2025-02-05 23:19:09 +0300

    worked on the ui

--- Dormly-Frontend/src/app/app.component.ts
+++ Dormly-Frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./user-home/home.component";
 import { NavbarComponent } from './navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

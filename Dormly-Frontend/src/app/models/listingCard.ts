ce4ce2cf4e5a036cbcd031140cccd9ce1357ea75
ajama0 <abasjama04@hotmail.com>
2025-02-06 16:20:29 +0300

    data model to handle dto return from spring

---
+++ Dormly-Frontend/src/app/models/listingCard.ts
@@ -0,0 +1,17 @@
+export interface listingCard{
+    listingId:number
+    title:string
+    price:number
+    description:string
+    brand:string
+    condition:string
+    isSold:boolean
+    location:string
+    category:string
+    availability:string
+    listingImage:URL
+    firstname:string  //the name of the seller
+    lastname:string
+    sellerProfilePicture:URL
+
+}

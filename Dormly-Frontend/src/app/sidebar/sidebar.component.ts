fd94a1f121894c98d025ebe6025d7d4f3d552c70
ajama0 <abasjama04@hotmail.com>
2025-02-16 22:05:01 +0300

    add sidebar

--- Dormly-Frontend/src/app/sidebar/sidebar.component.ts
+++ Dormly-Frontend/src/app/sidebar/sidebar.component.ts
@@ -1,20 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component, ɵresetCompiledComponents } from '@angular/core';
+import { Component, OnInit, ɵresetCompiledComponents } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ListingService } from '../service/listing/listing.service';
+import { CategoryDto } from '../models/CategoryDto';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
 
+  categories:Array<String> = []
+  categoriesReturned:boolean = false
+  
+  constructor(private listingService:ListingService){}
+  
+  
+  ngOnInit() :void{
+    this.findAllCategories()
+  }
 
-  //sidenav:boolean = false
+  ///we dont want categories statically placed in side navbar 
+  findAllCategories(){
+    console.log("entered")
+    this.listingService.findAllCategories()
+    .subscribe({
+      next:(res:CategoryDto[])=>{
+        console.log(res)
+        res.forEach(category=>this.categories.push(category.categoryName))
+        this.categoriesReturned = true
+        console.log("categories returned")
+        
+      },
+      error:(err:Error)=>{
+        console.log("error retrieving categories", err.message)
+      }
+    })
+  }
 
-
-  constructor(){}
+  //whenever the user clicks a category we bind category as a query parameter to the url
 
 
 

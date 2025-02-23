01bdb298585da32cc9399dc5bbc090fce5d489c9
ajama0 <abasjama04@hotmail.com>
2025-02-24 00:37:38 +0300

    complete websocket impl

--- Dormly-Frontend/src/app/messages/messages.component.ts
+++ Dormly-Frontend/src/app/messages/messages.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebSocketApiService } from '../websocket/web-socket-api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-messages',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit{
+
+  messages : any = []
+  constructor(private webSocket:WebSocketApiService){}
+
+  ngOnInit():void{
+  this.onMessageRecieved()
+
+  }
+   onMessageRecieved(){
+  this.webSocket.messageSubscription$.subscribe({
+    next:(message)=>{
+      if(message){
+        this.messages.push(message)
+      }
+    },
+    error:(error:Error)=>{
+      console.log(error)
+    }
+  })
+}
+
+
+connect(){
+  console.log("calling service class to connect to websocket")
+  this.webSocket.connect()
+}
+
+
+disconnect(){
+  this.webSocket.disconnect()
+}
+  
+  
+
+
+
 
 }
--- Dormly-Frontend/src/app/websocket/web-socket-api.service.ts
+++ Dormly-Frontend/src/app/websocket/web-socket-api.service.ts
@@ -1,9 +1,92 @@
 import { Injectable } from '@angular/core';
+import { TokenService } from '../auth/token/token.service';
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs';
+import { Message } from '../models/Message';
+import { MessagesComponent } from '../messages/messages.component';
+import { BehaviorSubject } from 'rxjs';
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketApiService {
+  //create a subject to communicate the responses back to the messages component
+  private subject = new BehaviorSubject("")
+  messageSubscription$ = this.subject.asObservable()
 
-  constructor() { }
+
+  destination:string = "/user/queue/chat"
+  brokerURL:string = "http://localhost:8099/ws"
+  stompClient:any
+  token!:string
+ 
+
+  constructor(private tokenService:TokenService) {
+    this.token = this.tokenService.token as string
+   }
+
+
+
+  connect(){
+    console.log("connecting to websocket...")
+    let ws = new SockJS(this.brokerURL)
+      this.stompClient = Stomp.over(ws)
+    
+    this.stompClient.connect({"Authorization" : "Bearer " + this.token}, () =>{
+
+        ///the stompclient takes 3 parameters which includes the headers and 2 callback functions,
+        ///the frames defined the handshake agreement of protocol switches, and once the event occurs we can now subscribe to the destination
+
+        this.stompClient.subscribe(this.destination, (message:any)=>{
+          //the subscribe also triggers a callback which means when a user subscribes to a destination, an event of a message could be returned
+          this.onMessageRecieved(message)
+
+        })
+
+    },
+    ///error callback
+    (error:Error | any)=>{
+    this.errorCallBack(error)
+    }
+  )
+
+
+
+  }
+  errorCallBack(error:Error):void{
+    console.log(error.message)
+    
+  }
+
+  disconnect(){
+    if(this.stompClient!==null){
+      this.stompClient.disconnect()
+      console.log("disconnected")
+    }
+    
+  }
+
+
+  onMessageRecieved(message:any) {
+   ///manual deserialization with websockets. using Json.parse() to convert the json into a javascript objecy
+    console.log("message recieved ", message)
+    this.subject.next(JSON.parse(message))
+    console.log("added message to the subject")
+
+  }
+
+
+  send(message:Message):void{
+    ///when the user sends a message, our in memory message broker in spring will automatically forward it to the destination the user is subscribed to
+    ///the user will recieve the message immediatley assuming the connection is still live - if not we persist the chat to the db
+    ///all messages are sent with the /app prefix and users are able to send messages
+    ///the message object includes the recieptent and the message itself.
+    ///websocket does not serialize the object into a json like HTTP does, hence we do it manually
+    this.stompClient.send("/app/chat/send", {"Authorization" : "Bearer " + this.token}, JSON.stringify(message))
+  }
+
+
 }

77fe5d82a9fcea3b4dfba74a257e9ca1eaec6c6f
ajama0 <abasjama04@hotmail.com>
2025-02-25 18:05:03 +0300

    impl message interceptor to validate token on CONNECT

---
+++ Dormly-Backend/src/main/java/com/example/Dormly/websocket/MessageInterceptor.java
@@ -0,0 +1,61 @@
+package com.example.Dormly.websocket;
+
+import com.example.Dormly.jwt.service.CustomUserDetailsService;
+import com.example.Dormly.jwt.service.JwtService;
+import jakarta.annotation.Nullable;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Objects;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class MessageInterceptor implements ChannelInterceptor {
+
+
+   private final JwtService jwtService;
+   private final CustomUserDetailsService customUserDetailsService;
+    /// this interceptor is used to intercept all STOMP connect frames
+    /// we validate the users token sent in the headers of the connect frame and sets the principal in the ws context,
+    /// this way we associate the websocket session with this specific user.
+    /// other frames like SEND,SUBSCRIBE use this principal as the authenticated user
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            /// this checks if the command being sent is a CONNECT command
+            /// a user will not be able to send any frames unless they are connected to a STOMP protocol
+            /// we check here to ensure the authorization header isn't null
+             String authHeader = Objects.requireNonNull(accessor.getHeader("Authorization")).toString();
+
+             if(authHeader.startsWith("Bearer ")) {
+
+                 String jwt = authHeader.substring(7);
+                 String username = jwtService.extractSubject(jwt);
+                 UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+                 // Access authentication header(s) and invoke accessor.setUser(user)
+                 UsernamePasswordAuthenticationToken authenticatedUser = new UsernamePasswordAuthenticationToken(userDetails,
+                         null,
+                         userDetails.getAuthorities());
+                 accessor.setUser(authenticatedUser);
+
+                 return message;
+             }
+
+        }
+        log.info("AuthHeader is null");
+        return null;
+    }
+}
--- Dormly-Backend/src/main/java/com/example/Dormly/websocket/WebSocketConfig.java
+++ Dormly-Backend/src/main/java/com/example/Dormly/websocket/WebSocketConfig.java
@@ -1,17 +1,23 @@
 package com.example.Dormly.websocket;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-     /// this method initiates the web socket connection, when a client wants to upgrade their
+    private final MessageInterceptor messageInterceptor;
+
+    /// this method initiates the web socket connection, when a client wants to upgrade their
     /// protocol from HTTP to WebSocket
-        /// . we also define the servers that can make initiate a websocket connection
+    /// . we also define the servers that can make initiate a websocket connection
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
@@ -24,10 +30,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     /// MessageMapping annotation methods, similar to how requestMapping routed the endpoint to the specific method
     /// the message broker is used to define the endpoint in which a user will be subscribed to
     /// we set the user as the prefix
-    ///
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/user");
         registry.setApplicationDestinationPrefixes("/app");
     }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(messageInterceptor);
+    }
+
+
 }
